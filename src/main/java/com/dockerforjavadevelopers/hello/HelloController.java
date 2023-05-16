efeba961bb5c582e08c115019b2d981e604c3745
WiryandiPomin <133739992+WiryandiPomin@users.noreply.github.com>
2023-05-16 12:42:33 +0700

    Add files via upload

---
+++ src/main/java/com/dockerforjavadevelopers/hello/HelloController.java
@@ -0,0 +1,22 @@
+package com.dockerforjavadevelopers.hello;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@RestController
+public class HelloController {
+    
+    Logger logger = LoggerFactory.getLogger(HelloController.class);
+
+    @GetMapping("/")
+    public String index() {
+        logger.trace("A TRACE Message");
+        logger.debug("A DEBUG Message");
+        logger.info("An INFO Message");
+        logger.warn("A WARN Message");
+        logger.error("An ERROR Message");
+        return "Hello World! World spring boot \n";
+    }
+    
+}
---
+++ src/test/java/com/dockerforjavadevelopers/hello/DummyTest.java
@@ -0,0 +1,13 @@
+package com.dockerforjavadevelopers.hello;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class DummyTest {
+
+    @Test
+    void aTest() {
+    assertEquals(true, true);
+  }
+}
